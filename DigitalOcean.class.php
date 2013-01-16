295dbd1f772bc248ffdc9a7e2738d12d3b1639cf
Remmelt Pit <remmelt@info.nl>
2013-01-16 16:24:17 +0100

    Added copyright information

--- DigitalOcean.class.php
+++ DigitalOcean.class.php
@@ -1,11 +1,15 @@
 <?php
 
 /*************************************************
-
-DigitalOcean PHP Class
-Author: Giacomo Tüfekci <giacomo.tuefekci@googlemail.com>
-Copyright (c): 2012 Is.It.Media, all rights reserved
-Version: 1.0.0 - 2012-10-26
+ * DigitalOcean PHP Class
+ *
+ * Author: Giacomo Tüfekci <giacomo.tuefekci@googlemail.com>
+ * Additions: Remmelt Pit <remmelt@gmail.com>
+ *
+ * Copyright (c): 2012 Is.It.Media, all rights reserved
+ * Version: 1.0.0 - 2012-10-26
+ * Version: 1.1.0 - 2013-01-16
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -19,12 +23,12 @@ Version: 1.0.0 - 2012-10-26
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-You may contact the author of DigitalOcean PHP Class by e-mail at:
-giacomo.tuefekci@googlemail.com
-
-The latest version of DigitalOcean PHP Class can be obtained from:
-https://github.com/tuefekci/DigitalOcean-PHP-Class
+ *
+ * You may contact the author of DigitalOcean PHP Class by e-mail at:
+ * giacomo.tuefekci@googlemail.com
+ *
+ * The latest version of DigitalOcean PHP Class can be obtained from:
+ * https://github.com/tuefekci/DigitalOcean-PHP-Class
  *************************************************/
 
 class DigitalOcean {
