9fbee2ab4419bb2ee18c9774e80190ea1c5daba3
Remmelt Pit <remmelt@remmelt.com>
2013-01-18 10:13:38 +0100

    This is the correct set of unit tests the refactored code should conform to

--- tests/DigitalOceanUnitTests.php
+++ tests/DigitalOceanUnitTests.php
@@ -44,7 +44,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testGetDroplets() {
-		$expectedParameter = 'https://api.digitalocean.com/droplets?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->getDroplets();
 	}
@@ -60,7 +60,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 
 	public function testShowDroplet() {
 		$dropletId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->showDroplet($dropletId);
 	}
@@ -70,7 +70,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 		$sizeId = 3;
 		$imageId = 2;
 		$regionId = 1;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/new?name=dropletName&size_id=3&image_id=2&region_id=1?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/new?name=dropletName&size_id=3&image_id=2&region_id=1&client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->newDroplet($name, $sizeId, $imageId, $regionId);
 	}
@@ -120,7 +120,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 	public function testResizeDroplet() {
 		$dropletId = 1234;
 		$sizeId = 1;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/resize/?size_id=1?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/resize/?size_id=1&client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->resizeDroplet($dropletId, $sizeId);
 	}
@@ -128,7 +128,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 	public function testTakeASnapshot() {
 		$dropletId = 1234;
 		$name = "snapshotName";
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/snapshot/?name=snapshotName?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/snapshot/?name=snapshotName&client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->takeASnapshot($dropletId, $name);
 	}
@@ -136,7 +136,7 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 	public function testRestore() {
 		$dropletId = 1234;
 		$imageId = 1;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/restore/?image_id=1?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/restore/?image_id=1&client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->restore($dropletId, $imageId);
 	}
@@ -144,41 +144,41 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 	public function testRebuild() {
 		$dropletId = 1234;
 		$imageId = 1;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/rebuild/?image_id=1?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/rebuild/?image_id=1&client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->rebuild($dropletId, $imageId);
 	}
 
 	public function testEnableBackups() {
 		$dropletId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/enable_backups?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/enable_backups/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->enableBackups($dropletId);
 	}
 
 	public function testDisableBackups() {
 		$dropletId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/disable_backups?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/disable_backups/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->disableBackups($dropletId);
 	}
 
 	public function testDestroy() {
 		$dropletId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/destroy?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/droplets/1234/destroy/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->destroy($dropletId);
 	}
 
 	public function testGetRegions() {
-		$expectedParameter = 'https://api.digitalocean.com/regions?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/regions/?client_id=test-client-id&api_key=test-api-key';
 		$json = '{"status":"OK","regions":[{"id":1,"name":"New York 1"},{"id":2,"name":"Amsterdam 1"}]}';
 		$this->initMock($expectedParameter, $json);
 		$this->digitalOcean->getRegions();
 	}
 
 	public function testGetImages() {
-		$expectedParameter = 'https://api.digitalocean.com/images?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/images/?client_id=test-client-id&api_key=test-api-key';
 		$json = '{"status":"OK","images":[{"id":429,"name":"Real Backup 10242011","distribution":"Ubuntu"},{"id":430,"name":"test233","distribution":"Ubuntu"},{"id":431,"name":"test888","distribution":"Ubuntu"},{"id":442,"name":"tesah22","distribution":"Ubuntu"},{"id":443,"name":"testah33","distribution":"Ubuntu"},{"id":444,"name":"testah44","distribution":"Ubuntu"},{"id":447,"name":"ahtest55","distribution":"Ubuntu"},{"id":448,"name":"ahtest66","distribution":"Ubuntu"},{"id":449,"name":"ahtest77","distribution":"Ubuntu"},{"id":458,"name":"Rails3-1Ruby1-9-2","distribution":"Ubuntu"},{"id":466,"name":"NYTD Backup 1-18-2012","distribution":"Ubuntu"},{"id":478,"name":"NLP Final","distribution":"Ubuntu"},{"id":540,"name":"API - Final","distribution":"Ubuntu"},{"id":577,"name":"test1-1","distribution":"Ubuntu"},{"id":578,"name":"alec snapshot1","distribution":"Ubuntu"}]}';
 		$this->initMock($expectedParameter, $json);
 		$this->digitalOcean->getImages();
@@ -186,54 +186,54 @@ class DigitalOceanUnitTests extends PHPUnit_Framework_TestCase {
 
 	public function testShowImage() {
 		$imageId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/images/1234?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/images/1234/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->showImage($imageId);
 	}
 
 	public function testDestroyImage() {
 		$imageId = 1234;
-		$expectedParameter = 'https://api.digitalocean.com/images/1234/destroy?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/images/1234/destroy/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->destroyImage($imageId);
 	}
 
 	public function testGetSSHKeys() {
-		$expectedParameter = 'https://api.digitalocean.com/ssh_keys?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->getSSHKeys();
 	}
 
 	public function testShowSSHKey() {
 		$sshKeyId = 321;
-		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->showSSHKey($sshKeyId);
 	}
 
 	public function testAddSSHKey() {
 		$sshKeyId = 321;
-		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/add?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/add/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->addSSHKey($sshKeyId);
 	}
 
 	public function testEditSSHKey() {
 		$sshKeyId = 321;
-		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/edit?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/edit/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->editSSHKey($sshKeyId);
 	}
 
 	public function testDestroySSHKey() {
 		$sshKeyId = 321;
-		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/destroy?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/ssh_keys/321/destroy/?client_id=test-client-id&api_key=test-api-key';
 		$this->initMock($expectedParameter);
 		$this->digitalOcean->destroySSHKey($sshKeyId);
 	}
 
 	public function testGetSizes() {
-		$expectedParameter = 'https://api.digitalocean.com/sizes?client_id=test-client-id&api_key=test-api-key';
+		$expectedParameter = 'https://api.digitalocean.com/sizes/?client_id=test-client-id&api_key=test-api-key';
 		$json = '{"status":"OK","sizes":[{"id":33,"name":"512MB"},{"id":34,"name":"1GB"},{"id":35,"name":"2GB"},{"id":36,"name":"4GB"},{"id":37,"name":"8GB"},{"id":38,"name":"16GB"}]}';
 		$this->initMock($expectedParameter, $json);
 		$this->digitalOcean->getSizes();
